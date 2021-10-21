1a6366f3630d7dfb496b884bf487d2435ef0fc81
tpichurina <krushinskayatanya@gmail.com>
2021-10-21 22:05:56 +0300

    HW7

---
+++ AutotestTestProject/AutotestTestProject/MenuTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+
+
+namespace AutotestTestProject
+{
+    [TestFixture]
+    public class MenuTests : TestBase
+    {
+        [Test]
+        public void MenuTest()
+        {
+            OpenLoginPage();
+            LoginAsAdmin();
+
+            IList<IWebElement> menuPoints = driver.FindElements(By.CssSelector("#box-apps-menu > li"));
+
+            for (int i = 0; i < menuPoints.Count; i++)
+            {
+                IWebElement point = driver.FindElements(By.CssSelector("#box-apps-menu > li"))[i];
+                point.Click();
+                point = driver.FindElements(By.CssSelector("#box-apps-menu > li"))[i];
+
+                IsElementsPresent("h1");
+
+                for (int y = 1; y < point.FindElements(By.CssSelector("li")).Count; y++)
+                {
+                    point.FindElements(By.CssSelector("li"))[y].Click();
+                    IsElementsPresent("h1");
+                    point = driver.FindElements(By.CssSelector("#box-apps-menu > li"))[i];
+                }
+            }
+        }
+    }
+}
---
+++ AutotestTestProject/AutotestTestProject/TestBase.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace AutotestTestProject
+{
+    public class TestBase
+    {
+        protected IWebDriver driver;
+        protected WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            //driver = new InternetExplorerDriver();
+            //driver = new FirefoxDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+
+        protected void LoginAsAdmin()
+        {
+            driver.FindElement(By.Name("username")).Clear();
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).Clear();
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+        }
+
+        protected void OpenLoginPage()
+        {
+            driver.Url = "http://localhost/litecart/admin";
+        }
+
+        protected void Timeout(int s = 5)
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(s);
+        }
+
+        protected void IsElementsPresent(string selector)
+        {
+            driver.FindElement(By.CssSelector(selector));
+        }
+    }
+}
