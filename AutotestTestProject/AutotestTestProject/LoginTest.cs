48f94ca56103151eac3a617e1a95486ac9ccb148
tpichurina <krushinskayatanya@gmail.com>
2021-10-21 22:06:24 +0300

    refactoring

--- AutotestTestProject/AutotestTestProject/LoginTest.cs
+++ AutotestTestProject/AutotestTestProject/LoginTest.cs
@@ -1,40 +1,15 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using System;
-using NUnit.Framework;
-using OpenQA.Selenium.Support.UI;
-using SeleniumExtras.WaitHelpers;
+﻿using NUnit.Framework;
 
 namespace AutotestTestProject
 {
     [TestFixture]
-    public class LoginTests
+    public class LoginTests : TestBase
     {
-        private IWebDriver driver;
-        private WebDriverWait wait;
-
-        [SetUp]
-        public void start()
-        {
-            driver = new ChromeDriver();
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-        }
-
         [Test]
         public void LoginTest()
         {
-            driver.Url = "http://localhost/litecart/admin";
-            driver.FindElement(By.Name("username")).SendKeys("admin");
-            driver.FindElement(By.Name("password")).SendKeys("admin");
-
-            driver.FindElement(By.Name("login")).Click();
-        }
-
-        [TearDown]
-        public void stop()
-        {
-            driver.Quit();
-            driver = null;
+            OpenLoginPage();
+            LoginAsAdmin();
         }
     }
 }
